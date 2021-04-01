d16fba4b5c96b6b31cf2eafad32f19245da05973
Robin Söderholm <robin.soderholm87@gmail.com>
2021-04-02 00:09:49 +0200

    Broke out extensions to its own class

---
+++ FileExtensions/FileExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace FileSorter.Extensions
+{
+    public static class FileExtensions
+    {
+        public static string[] ZipExtensions = new string[] { ".zip", ".rar", ".7z" };
+        public static string[] ImageExtensions = new string[] { ".png", ".jpg", ".gif", ".jpeg", ".eps", ".bmp", ".tif", ".tiff" };
+        public static string[] InstallerExtensions = new string[] { ".exe", ".msi" };
+        public static string[] BookExtensions = new string[] { ".epub", ".mobi", ".pdf" };
+        public static string[] DocumentExtensions = new string[] { ".txt", ".doc", ".docx", ".rtf" };
+        public static string[] VideoExtensions = new string[] { ".avi", ".mp4", ".mpeg", ".mpg" };
+    }
+}
